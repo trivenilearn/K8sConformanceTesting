c82f8d261f6a44767c1b6d56a22358e4f1357797
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-07-26 23:18:39 +0530

    Create deployTestCluster2.sh

---
+++ deployTestCluster2.sh
@@ -0,0 +1,7 @@
+# For ease of use, set this local environment variable so kubectl uses the downloaded file:
+
+export KUBECONFIG=$(pwd)/kubeconfig
+
+# Run this command to list all Kubernetes nodes
+
+kubectl get nodes
